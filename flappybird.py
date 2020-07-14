227c00e6243f08afa35b0b4f8587071604794b1e
Witold Banach <witoldbanach99@gmail.com>
2020-07-14 16:38:31 +0200

    created basic logic of the game

--- flappybird.py
+++ flappybird.py
@@ -1,51 +1,190 @@
 import pygame
 import random
+import time
 
 pygame.init()
 
-scr_width, scr_height=640, 480
-screen = pygame.display.set_mode((scr_width, scr_height))
+# scr_width, scr_height=640, 480
+# screen = pygame.display.set_mode((scr_width, scr_height))
+
 class Bird:
-	def __init__(self,x,y,velocity):
+	def __init__(self,x,y):
 		self.image = pygame.image.load("images/bird.png")
+		self.image=pygame.transform.scale(self.image,(40,40))
+		self.dead=False
 		self.rect =	self.image.get_rect()
-		self.rect.center=scr_width/3, scr_height/2 
-		self.x=x
-		self.y=y
-		self.vel=velocity
+		self.rect.left=x
+		self.rect.top= y
+		self.is_alive=True
+		self.vel=0
+		self.time=time.perf_counter()
+		self.gravity=0.1
+	def move(self):
+		if self.dead is True:
+			pass
+		if self.time<time.perf_counter()-0.015:
+			self.vel+=self.gravity
+			self.time=time.perf_counter()
+		self.rect.top+=self.vel
 
-	def move():
-		pass
-
-	def jump():
-		pass
+	def jump(self):
+		if self.is_alive is True:
+			self.vel=-2
+		else:
+			pass
 
 	def draw(self, win):
-		screen.blit(self.image,self.rect)
+		win.blit(self.image,self.rect)
 
 class Pipe:
-	SPACE = 50
+	def __init__(self,x,y, is_bottom_pipe):
+		self.image = pygame.image.load("images/pipe.png")
+		self.image = pygame.transform.scale(self.image, (90,450))
+		self.rect =	self.image.get_rect()
+		self.is_bottom_pipe=is_bottom_pipe
+		self.rect.left=x
+		self.rect.top=y
+		if is_bottom_pipe is False:
+			self.image = pygame.transform.rotate(self.image,180)
+
+	def move(self,top=None,left=None):
+		self.rect.left-=1
+
+	def rotate_pipe(self):
+		self.image = pygame.transform.rotate(self.image,180)
+
+	def draw(self, win):
+		win.blit(self.image,self.rect)
+
+class Base:
 	def __init__(self):
-		self.x=x
+		self.image=pygame.image.load("images/base.png")
+		self.image=pygame.transform.scale(self.image,(800,100))
+		self.rect = self.image.get_rect()
+		self.rect.top=400
+		self.rect.left=0
+	def move(self):
+		if self.rect.left<-100:
+			self.rect.left=0
+		self.rect.left-=1
+	def draw(self,win):
+		win.blit(self.image,self.rect)
+
+class Background:
+	def __init__(self):
+		self.image=pygame.image.load("images/background.png")
+	def draw(self,win):
+		win.blit(self.image, [0, 0])
+
+class Text:
+	def __init__(self, text):
+		pygame.font.init()          
+		self.myfont = pygame.font.SysFont('Comic Sans MS', 20)
+		self.textsurface = self.myfont.render(text, False, (0, 0, 0))
+	def update(self, text):
+		self.textsurface = self.myfont.render(text, False, (0, 0, 0))
+	def draw(self,win):
+		win.blit(self.textsurface,(0,0))
+
+
+class Game:
+	def __init__(self,width=640,height=480):
 		self.width=width
+		self.height=height
+		self.screen=pygame.display.set_mode((self.width, height))
+		self.bird=Bird(width/3,height/2)
+		self.nr_of_pipes=3
+		self.space_between_pipes=130
+		self.bot_pipe=[Pipe(width,height,True) for i in range(self.nr_of_pipes)]
+		self.top_pipe=[Pipe(width,height,False) for i in range(self.nr_of_pipes)]
+		self.base=Base()
+		self.bg=Background()
+		self.score_last=-1
+		self.score_value=0
+		self.text=Text(f'Score: {self.score_value}')
 
-	def move():
-		pass
+	def create_pipes(self):
+		for i in range(self.nr_of_pipes):
+			y_random=random.randint(-400,-200)
+			self.bot_pipe[i].rect.top=y_random+self.space_between_pipes+self.top_pipe[i].rect.height
+			self.top_pipe[i].rect.top=y_random
+			self.bot_pipe[i].rect.left=self.width+i*(self.width+self.bot_pipe[i].rect.width)/self.nr_of_pipes
+			self.top_pipe[i].rect.left=self.width+i*(self.width+self.bot_pipe[i].rect.width)/self.nr_of_pipes
 
-	def draw():
-		pass
+	def is_collison(self):
+		for i in range(self.nr_of_pipes):
+			if  (
+					self.bird.rect.colliderect(self.top_pipe[i].rect) or 
+			    	self.bird.rect.colliderect(self.bot_pipe[i].rect) or 
+					self.bird.rect.colliderect(self.base.rect)
+				):
+				self.bird.is_alive=False
+				return True
+			if  (
+					self.bird.rect.left>self.top_pipe[i].rect.left and
+					(
+						(
+							self.score_last==2 and
+							i==0
+						) or
+						(
+							self.score_last==i-1
+						)
+					)
+				):
+				self.score_last = i
+				self.score_value+=1
 
-bg_img = pygame.image.load("images/background.png")
-ptak = Bird(100,100,10)
-running = True
-while running:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-        screen.blit(bg_img, [0, 0])
-        ptak.draw(screen)
-        pygame.display.flip()
+	def move(self):
+		self.bird.move()
+		for i in range(self.nr_of_pipes):
+			self.bot_pipe[i].move(self.bot_pipe[i].rect.top,self.bot_pipe[i].rect.left+i*self.width/self.nr_of_pipes)
+			self.top_pipe[i].move(self.top_pipe[i].rect.top,self.top_pipe[i].rect.left+i*self.width/self.nr_of_pipes)
+			if self.bot_pipe[i].rect.left+self.bot_pipe[i].rect.width<0:
+				self.bot_pipe[i].rect.left=self.width
+				self.top_pipe[i].rect.left=self.width
 
+				y_random=random.randint(-400,-200)
+				self.bot_pipe[i].rect.top=y_random+self.space_between_pipes+self.top_pipe[i].rect.height
+				self.top_pipe[i].rect.top=y_random
+
+		self.base.move()
+		self.text.update(f'Score: {self.score_value}')
+
+	def draw(self):
+		self.bg.draw(self.screen)
+		for i in range((self.nr_of_pipes)):
+			self.bot_pipe[i].draw(self.screen)
+			self.top_pipe[i].draw(self.screen)
+		self.base.draw(self.screen)
+		self.bird.draw(self.screen)
+		self.text.draw(self.screen) 
+
+	def restart(self):
+		self.bird=Bird(self.width/3,self.height/2)
+		self.create_pipes()
+		self.score_last=-1
+		self.score_value=0
+
+	def game_on(self):
+		self.restart()
+		running=True
+		while running:
+			for event in pygame.event.get():
+				if event.type == pygame.QUIT:
+					running = False
+				if event.type ==pygame.KEYDOWN:
+					self.bird.jump() 
+			self.move()
+			self.draw()
+			if self.is_collison():	
+				time.sleep(1)
+				self.restart()
+
+			pygame.display.update()
+			
+
+gierka=Game()
+gierka.game_on()
 
 pygame.quit()
-
